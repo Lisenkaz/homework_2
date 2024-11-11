68ddc796b6902d70fe1524ecd2939350bb573fef
Zimina Olesia <ol3siazim@yandex.ru>
2024-11-11 14:34:32 +0300

    Добавление функции extract_dependencies

--- hw2.py
+++ hw2.py
@@ -12,7 +12,38 @@ class DependencyVisualizer:  # Определение класса DependencyVis
         self.repo_url = config['repo_url']  # Сохранение URL-адреса репозитория из конфигурации.
     # Метод для извлечения зависимостей из пакета.
     def extract_dependencies(self, package_path, depth=0):
-        
+        if depth > self.max_depth:  # Проверка, не превышает ли текущая глубина максимальную.
+            return {}  # Если превышает, вернуть пустой словарь (нет зависимостей).
+        dependencies = {}  # Инициализация словаря для хранения зависимостей.
+        try:
+            # Открытие ZIP-архива с пакетом.
+            with zipfile.ZipFile(package_path) as z:
+                # Проверка наличия файла .nuspec в архиве.
+                nuspec_file = [f for f in z.namelist() if f.endswith('.nuspec')]
+                if not nuspec_file:
+                    print(f"Ошибка: файл '.nuspec' не найден в {package_path}.")
+                    return dependencies  # Возвращаем пустой словарь, если файл не найден.
+                # Открытие файла спецификации пакета (.nuspec).
+                with z.open(nuspec_file[0]) as f:
+                    tree = ET.parse(f)
+                    root = tree.getroot()  # Получение корневого элемента XML.
+                    # Пространства имен в файле .nuspec.
+                    namespaces = {'ns': 'http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd'}
+                    # Поиск всех элементов зависимости в XML.
+                    for dependency in root.findall('.//ns:dependency', namespaces):
+                        dep_id = dependency.get('id')  # Получение идентификатора зависимости.
+                        dep_version = dependency.get('version')  # Получение версии зависимости.
+                        if dep_id is None or dep_version is None:
+                            print("Ошибка: Не указаны идентификатор или версия зависимости.")
+                            continue  # Пропускаем текущую зависимость, если данные отсутствуют.
+                        dependencies[dep_id] = dep_version  # Сохраняем зависимость и её версию в словаре.
+        except FileNotFoundError as e:
+            print(f"Ошибка: файл не найден - {e}")  # Обработка ошибки, если архив не найден.
+        except zipfile.BadZipFile:
+            print(f"Ошибка: файл '{package_path}' не является корректным ZIP-архивом.")  # Обработка ошибки для некорректных архивов.
+        except Exception as e:
+            print(f"Произошла непредвиденная ошибка: {e}")  # Общая обработка ошибок.
+        return dependencies  # Возврат собранного словаря зависимостей.
     # Метод для создания графа.
     def generate_graph(self, dependencies, depth=0):
 
